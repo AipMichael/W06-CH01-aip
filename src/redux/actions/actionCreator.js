8bd212dd0d65940297564df76945e08f0ca91cf4
AipBorges <mpbds@uic.es>
2021-11-01 18:16:50 +0100

    cleaned some typos and non used words and variables from files

--- src/redux/actions/actionCreator.js
+++ src/redux/actions/actionCreator.js
@@ -1,6 +1,6 @@
 import actionTypes from "./actionTypes";
 
-export const createTasksAction = (task) => ({
+export const createTaskAction = (task) => ({
   type: actionTypes.createTask,
   task,
 });
