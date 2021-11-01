fd8458b3fd376f444b5ba36e25d8c5e09c9be158
AipBorges <mpbds@uic.es>
2021-11-01 16:09:55 +0100

    deleted unused logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Aip Am An Aip Api App</h1>
+    </div>
+  );
 }
 
 export default App;
