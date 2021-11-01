c192c0af5fa2b9d2a935b53971ff19dce02f2e79
AipBorges <mpbds@uic.es>
2021-11-01 18:14:36 +0100

    included new values in store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,7 +1,15 @@
-const { createStore } = require("redux");
+import momReducer from "../reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+const { createStore, applyMiddleware } = require("redux");
 
 const configureStore = (initialValues) => {
-  return createStore();
+  return createStore(
+    momReducer,
+    initialValues,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
 };
 
 export default configureStore;
